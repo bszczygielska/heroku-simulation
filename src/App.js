ed31d4ee9633d6b84e9413ac49bca2b371ab2dd4
Basia <barbara.szczgielska@gmail.com>
2018-09-25 00:24:40 +0200

    heroku

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from "react";
+import * as io from 'socket.io-client';
+import 'antd/dist/antd.css';
+
+const socket = io('https://light-manager-client.herokuapp.com/socket');
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      text: '',
+    };
+  }
+
+  componentWillMount() {
+    socket.on('toSimulation', message => {
+      this.setState({text: message});
+    })
+  }
+
+  onClickHandler() {
+    socket.emit('fromSimulation')
+  }
+
+  render() {
+    return (
+      <div style={{textAlign: "center"}}>
+        <button onClick={this.onClickHandler}>Click to emit to server</button>
+        Message from client: {this.state.text}
+      </div>
+    );
+  }
+}
+
+export default App;
