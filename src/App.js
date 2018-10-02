844a92cd3d4cf12578a64552e38030faac2e5d87
Basia <barbara.szczgielska@gmail.com>
2018-10-02 22:28:44 +0200

    colors

--- src/App.js
+++ src/App.js
@@ -73,7 +73,7 @@ class App extends React.Component {
     return <Card
       key={`room_${name}`}
       title={name}
-      style={{ display: 'flex', flexDirection: 'column', margin: 10}}
+      style={{ backgroundColor: 'lightgrey', display: 'flex', flexDirection: 'column', margin: 10}}
     >
       {this.decideWhatRender(room, path)}
     </Card>
